85c75ca7ceda029f6813309ab0976dabe76adfe9
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-09 11:47:50 +0530

    returning an empty array maybe endpoint chenged

--- App.js
+++ App.js
@@ -408,107 +408,184 @@
 // //make this component available to the app
 // export default MyComponent;
 
+// //import liraries
+// import React, {Component, useEffect, useState} from 'react';
+// import {View, Text, StyleSheet, Button, Modal, TextInput} from 'react-native';
+
+// // create a component
+// const MyComponent = () => {
+//   const [showModal, setShowModal] = useState(false);
+//   const [data, setData] = useState([]);
+//   const [selectedUser, setSelectedUser] = useState(undefined);
+//   const getApidata = async () => {
+//     const url = 'http://192.168.111.1:3000/user';
+//     let result = await fetch(url);
+//     result = await result.json();
+//     // console.warn(result);
+//     if (result) {
+//       setData(result);
+//     }
+//   };
+
+//   const updateUser = data => {
+//     setShowModal(true);
+//     setSelectedUser(data);
+//   };
+
+//   useEffect(() => {
+//     getApidata();
+//   }, []);
+
+//   return (
+//     <View>
+//       <View style={[styles.apiview, {marginTop: 10}]}>
+//         <Text style={styles.apitext}>Name</Text>
+//         <Text style={styles.apitext}>Update</Text>
+//       </View>
+
+//       {data.length ? (
+//         data.map((item, id) => {
+//           return (
+//             <View key={id} style={styles.apiview}>
+//               <Text style={styles.apitext}>{item.name}</Text>
+//               <Button title="update" onPress={() => updateUser(item)} />
+//             </View>
+//           );
+//         })
+//       ) : (
+//         <View>
+//           <Text>no data found</Text>
+//         </View>
+//       )}
+
+//       <Modal transparent={true} visible={showModal}>
+//         <UserModal setShowModal={setShowModal} selectedUser={selectedUser} getApidata={getApidata} />
+//       </Modal>
+//     </View>
+//   );
+// };
+
+// const UserModal = props => {
+
+//   //console.warn(props.selectedUser)
+//   const [name, setName] = useState()
+//   const [age, setAge] = useState()
+//   //console.warn(age)
+//   useEffect(()=>{
+//     if(props.selectedUser){
+//       setName(props.selectedUser.name)
+//       setAge(props.selectedUser.age.toString())
+//     }
+// },[props.selectedUser])
+
+// const updateData = async ()=> {
+//   //console.warn(name,age)
+//      const url = "http://192.168.111.1:3000/user";
+//      const id = props.selectedUser.id;
+//      let result =await fetch (`${url}/${id}`,{
+//        method:'Put',
+//        headers:{
+//          "Content-Type":"application/json"
+//        },
+//        body:JSON.stringify({name,age})
+//     });
+
+//      result= await result.json();
+
+//      if (result){
+//       // console.warn(name, age);
+//        props.getApidata();
+//        props.setShowModal(false);
+//      }
+// }
+
+//   return (
+//     <View style={styles.CenteredView}>
+//       <View style={styles.modalView}>
+//         <TextInput style={styles.textinput} value={name} onChangeText={(text)=>setName(text)} />
+//         <TextInput style={styles.textinput} value={age}  onChangeText={(text)=>setAge(text)}/>
+//         <View style={{marginBottom:10,}}>
+//           <Button title="Update"  onPress={updateData}/>
+//         </View>
+//         <Button title="close" onPress={() => props.setShowModal(false)} />
+//       </View>
+//     </View>
+//   );
+// };
+
+// // define your styles
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     // justifyContent: 'center',
+//     // alignItems: 'center',
+//     // backgroundColor: '#2c3e50',
+//   },
+//   apiview: {
+//     backgroundColor: 'orange',
+//     marginBottom: 10,
+//     padding: 15,
+//     borderRadius: 10,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//   },
+//   apitext: {
+//     fontSize: 20,
+//     color: 'white',
+//   },
+//   CenteredView: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   modalView: {
+//     backgroundColor: '#fff',
+//     padding: 40,
+//     borderRadius: 20,
+//     shadowColor: 'black',
+//     shadowOpacity: 0.6,
+//     elevation: 5,
+//   },
+//   textinput: {
+//     borderWidth: 1,
+//     borderRadius: 10,
+//     borderColor: 'black',
+//     width: 300,
+//     marginBottom: 15,
+//   },
+// });
+
+// //make this component available to the app
+// export default MyComponent;
+
 //import liraries
-import React, {Component, useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Button, Modal, TextInput} from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 // create a component
 const MyComponent = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [data, setData] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(undefined);
-  const getApidata = async () => {
-    const url = 'http://192.168.111.1:3000/user';
+
+  const [data,setData]= useState([]);
+
+  const search =async (text)=>{
+    //console.warn(text);
+    const url = `http://192.168.111.1:3000/user?name=${text}`;
     let result = await fetch(url);
-    result = await result.json();
-    // console.warn(result);
-    if (result) {
-      setData(result);
+    result =  await result.json();
+    //console.warn(result);
+    if (result){
+    setData(result)
     }
-  };
-
-  const updateUser = data => {
-    setShowModal(true);
-    setSelectedUser(data);
-  };
-
-  useEffect(() => {
-    getApidata();
-  }, []);
-
+  }
   return (
     <View>
-      <View style={[styles.apiview, {marginTop: 10}]}>
-        <Text style={styles.apitext}>Name</Text>
-        <Text style={styles.apitext}>Update</Text>
+      <View style={{marginTop:15,}}>
+        <TextInput style={styles.TextInput} placeholder='search' onChangeText={(text)=>search(text)}/>  
       </View>
-
-      {data.length ? (
-        data.map((item, id) => {
-          return (
-            <View key={id} style={styles.apiview}>
-              <Text style={styles.apitext}>{item.name}</Text>
-              <Button title="update" onPress={() => updateUser(item)} />
-            </View>
-          );
-        })
-      ) : (
-        <View>
-          <Text>no data found</Text>
-        </View>
-      )}
-
-      <Modal transparent={true} visible={showModal}>
-        <UserModal setShowModal={setShowModal} selectedUser={selectedUser} getApidata={getApidata} />
-      </Modal>
-    </View>
-  );
-};
-
-const UserModal = props => {
-
-  //console.warn(props.selectedUser)
-  const [name, setName] = useState()
-  const [age, setAge] = useState()
-  //console.warn(age)
-  useEffect(()=>{
-    if(props.selectedUser){
-      setName(props.selectedUser.name)
-      setAge(props.selectedUser.age.toString())
-    }
-},[props.selectedUser])
-
-const updateData = async ()=> {
-  //console.warn(name,age)
-     const url = "http://192.168.111.1:3000/user";
-     const id = props.selectedUser.id;
-     let result =await fetch (`${url}/${id}`,{
-       method:'Put',
-       headers:{
-         "Content-Type":"application/json"
-       },
-       body:JSON.stringify({name,age})
-    });
-
-     result= await result.json();
-
-     if (result){
-      // console.warn(name, age);
-       props.getApidata();
-       props.setShowModal(false);
-     }
-}
-
-  return (
-    <View style={styles.CenteredView}>
-      <View style={styles.modalView}>
-        <TextInput style={styles.textinput} value={name} onChangeText={(text)=>setName(text)} />
-        <TextInput style={styles.textinput} value={age}  onChangeText={(text)=>setAge(text)}/>
-        <View style={{marginBottom:10,}}>
-          <Button title="Update"  onPress={updateData}/>
-        </View>
-        <Button title="close" onPress={() => props.setShowModal(false)} />
-      </View>
+      {
+        data.length ? data.map((item,id)=><View key={id}><Text>{item.name}</Text></View>):null
+      }
     </View>
   );
 };
@@ -516,44 +593,18 @@ const updateData = async ()=> {
 // define your styles
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // backgroundColor: '#2c3e50',
-  },
-  apiview: {
-    backgroundColor: 'orange',
-    marginBottom: 10,
-    padding: 15,
-    borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  apitext: {
-    fontSize: 20,
-    color: 'white',
-  },
-  CenteredView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    //backgroundColor: '#2c3e50',
   },
-  modalView: {
-    backgroundColor: '#fff',
-    padding: 40,
-    borderRadius: 20,
-    shadowColor: 'black',
-    shadowOpacity: 0.6,
-    elevation: 5,
-  },
-  textinput: {
-    borderWidth: 1,
-    borderRadius: 10,
-    borderColor: 'black',
-    width: 300,
-    marginBottom: 15,
-  },
+  TextInput:{
+    borderColor:'black',
+    borderWidth:1,
+    borderRadius:15,
+    margin:10,
+    padding:10,
+  }
 });
 
 //make this component available to the app
